dde4c463cbb4475e5649fa33606cd62abd40e335
rejuvyesh <mail@rejuvyesh.com>
2019-03-20 10:12:05 -0700

    Files generated by PkgTemplates

---
+++ src/AA222Project1_2019.jl
@@ -0,0 +1,5 @@
+module AA222Project1_2019
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AA222Project1_2019
+using Test
+
+@testset "AA222Project1_2019.jl" begin
+    # Write your own tests here.
+end
